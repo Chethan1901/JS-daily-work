c42fd71a73b42bc84e82955915aeb217308258f1
Chethan1901 <chethanreddy.akkati123@gmail.com>
2022-07-22 16:54:21 +0530

    string

--- DT.js
+++ DT.js
@@ -42,6 +42,42 @@
 // console.log(a)
 // console.log(b)
 
+//methods of string
+//charAt()
+// let chat = "hi, good morning"
+// let letter = chat.charAt(9);
+// let lastone = chat.charAt(chat.length-1);
+// console.log(letter)
+// console.log(lastone)
 
+//concat 
+// let T1 = "rope";
+// let T2 = "way"
+// let T3 = T1.concat(T2);
+// let T4 = T1.concat(" ", T2);
+// console.log(T4)
 
+//inclde
+// let A = "abcdef"
+// let result = A.includes("e");
+// let result = A.includes("g");
+// console.log(result)
+
+//indexof
+// let Act = "vscode is a integrated development environment"
+// let result = Act.indexOf("is")
+// let result = Act.indexOf("Is")
+// console.log(result)
+
+//length
+// let Act = "vscode is a integrated development environment"
+// console.log(Act.length)
+
+//repeat
+// let Name = "Rehmath"
+// console.log(Name.repeat(10))
+
+//replace 
+// let text = "chekri is a good boy";
+// console.log(text.replace(/chekri|good/gi, "bad"))
 
