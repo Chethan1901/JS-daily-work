ebb8fbda0e30d7b7a30f782296658aaa7181d7af
Chethan1901 <chethanreddy.akkati123@gmail.com>
2022-07-22 11:20:34 +0530

    july 22

--- DT.js
+++ DT.js
@@ -15,6 +15,33 @@
 // var b = '"joker"'
 // console.log(a+b)
 
-let str = "Apple, Banana, Kiwi";
-let part = str.slice(7, 13);
-console.log(part)
+//slice
+// let str = "Apple, Banana, Kiwi";
+// let part = str.slice(7, 13);
+// console.log(part)
+//JavaScript program to swap two variables
+
+//swap
+// let a = 5, b = 6;
+// [a, b] = [b, a];
+// console.log(`${a} ${b}`);
+
+
+// var a = 1, b = 5;
+// a=a+b;
+// b=a-b;
+// a=a-b;
+// console.log(a,b)
+
+
+// let a = 1;
+// let b = 5;
+// a = a ^ b
+// b = a ^ b
+// a = a ^ b
+// console.log(a)
+// console.log(b)
+
+
+
+
