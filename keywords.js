55c78636ec2e4d994f545353934fc9963e77ef37
Chethan1901 <chethanreddy.akkati123@gmail.com>
2022-07-23 11:51:26 +0530

    keywors

---
+++ keywords.js
@@ -0,0 +1,81 @@
+// var a = 3 
+// console.log(a)
+// {
+//     var a = 4
+//     console.log(a)
+//     a = 5
+// }
+// console.log(a)
+
+//let 
+// let a  = 4
+// console.log(a)
+// {
+//     a = 5 
+//     console.log(a)
+// }
+
+
+// let a  = 4
+// console.log(a)
+// {
+//    let a = 5 
+//     console.log(a)
+// }
+// console.log(a)
+
+// let a  = 4
+// console.log(a)
+// {
+//    let a = 5 
+//     console.log(a)
+// }
+//    a = 7
+// console.log(a)
+
+//  var b  = 4
+// console.log(b)
+// {
+//    let a = 5 
+//     console.log(a)
+//     var b = 7
+//     console.log(b)
+// }
+// let a = 6
+// console.log(a)
+
+//const
+// / no redefining
+// const a = 5
+// const a =6
+// console.log(a)
+
+// no reinstializing 
+// var a =5
+// const a = 5
+// console.log(a)
+
+// const a = 1
+// console.log(a)
+// {
+//     let a = 2
+//     console.log(a)
+// }
+// var b = 3
+// console.log(b)
+ 
+// imp
+// let  a = 1;
+// {
+//     console.log(a)
+//      {
+//            console.log(b)
+//          const b = 9;
+//         // console.log(a)
+//     }
+//      console.log(a)
+// }
+//  console.log(a)
+
+
+// Arrays
