eed47867843b49eae66f22210047530383970cdc
Chethan1901 <chethanreddy.akkati123@gmail.com>
2022-07-23 11:47:21 +0530

    Arrays

--- DT.js
+++ DT.js
@@ -81,3 +81,95 @@
 // let text = "chekri is a good boy";
 // console.log(text.replace(/chekri|good/gi, "bad"))
 
+// local / global scope
+
+// var a = 3 
+// console.log(a)
+// {
+//     var a = 4
+//     console.log(a)
+//     a = 5
+// }
+// console.log(a)
+
+//let 
+// let a  = 4
+// console.log(a)
+// {
+//     a = 5 
+//     console.log(a)
+// }
+
+
+// let a  = 4
+// console.log(a)
+// {
+//    let a = 5 
+//     console.log(a)
+// }
+// console.log(a)
+
+// let a  = 4
+// console.log(a)
+// {
+//    let a = 5 
+//     console.log(a)
+// }
+//    a = 7
+// console.log(a)
+
+//  var b  = 4
+// console.log(b)
+// {
+//    let a = 5 
+//     console.log(a)
+//     var b = 7
+//     console.log(b)
+// }
+// let a = 6
+// console.log(a)
+
+//const
+// / no redefining
+// const a = 5
+// const a =6
+// console.log(a)
+
+// no reinstializing 
+// var a =5
+// const a = 5
+// console.log(a)
+
+// const a = 1
+// console.log(a)
+// {
+//     let a = 2
+//     console.log(a)
+// }
+// var b = 3
+// console.log(b)
+ 
+// imp
+// let  a = 1;
+// {
+//     console.log(a)
+//      {
+//            console.log(b)
+//          const b = 9;
+//         // console.log(a)
+//     }
+//      console.log(a)
+// }
+//  console.log(a)
+
+
+// Arrays
+
+let a =["a",'b','c','d','e']
+console.log(a)
+console.log(a.length)
+console.log(a[0])
+console.log(a.slice(0,3))
+console.log()
+console.log()
+
