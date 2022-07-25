18a945018d5504b54a6921b941a20ce371b03afa
Chethan1901 <chethanreddy.akkati123@gmail.com>
2022-07-25 10:23:11 +0530

    conditions

---
+++ conditions.js
@@ -0,0 +1,114 @@
+// Control Flow
+// 1. Sequential
+// 2. Conditions
+// 3. Iterations
+
+// Condition 
+// if condition
+// if - else condition
+// if - else if - else  conditions
+// // Nested conditions
+
+// if (5) {
+//     console.log("statement")
+// }
+
+// if("zero") {
+//     console.log("statement")
+// }
+
+// if([]) {
+//     console.log("statement")
+// }
+
+// if(0) {
+//     console.log("statement")
+// }
+
+// if ((0) && []) {
+//     console.log("one")
+// }
+
+// if (8&7) {
+//     console.log("tw0")
+// }
+
+// if ([1,2]==[2,1])
+// {
+//     console.log("sndvahsv")
+// }
+
+// if ((2 =='2') | ((2) == 2.0)){
+//     console.log("typecasting")
+// }
+
+// if ((4^2)<<(6|7)){
+//     console.log("hey")
+// }
+
+// var a=3,b=5
+//    if(a&b | b|a){
+//     console.log("jgdhfj")
+//    }
+
+//    console.log(a&b | b|a)
+
+// var a=5,b=7
+// if (a>=b &  a<b ){
+//     console.log()
+// }
+
+// if ("abc" === "Abc"){    //try it with == & ===
+//     console.log("hbdskhk")
+// }
+
+// let a = 0                      // use strings as well         // positive negative  
+// if (a>0){
+//     console.log("positive")
+// }
+// else if (a<0){
+// console.log("negatiive")
+// }
+
+// else {
+//     console.log("zero")
+// }
+
+// let a = -4                                  //even odd              
+//  if ((a%3)==0){
+//     console.log("of 3")
+//     }
+    
+//   else  if ((a%5)==0){
+//         console.log("0f 5")
+//         }
+// else {
+//     console.log("not questioned")
+// }
+
+// let a = 5                                     //use strings as well 
+// let b = 5
+
+// if(a>b){
+// console.log("a is greater")
+// }
+// else if (b>a)
+// {
+//     console.log("b is greater")
+// }
+// else {
+//     console.log("equal")
+// }
+
+// function compare(num1, num2, num3) {
+// 	if (num3 > num2 && num1 > num3) {
+// 		return num1 + ' is the biggest number';
+// 	} else if (num2 > num1 && num2 > num3) {
+// 		return num2 + ' is the biggest number';
+// 	} else {
+// 		return num3 + ' is the biggest number';
+// 	}
+// }
+// var results = compare(10, 20, 30);
+// console.log(results);
+
