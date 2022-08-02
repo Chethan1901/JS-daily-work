a2eb8aaa90a1bc960167dac3ef342c516c23af43
Chethan1901 <chethanreddy.akkati123@gmail.com>
2022-08-02 13:10:37 +0530

    Aug2

--- function.js
+++ function.js
@@ -32,3 +32,61 @@
 // return age > 23
 // }
 
+
+// 0 1 1 2 3 5 8 13 21............
+
+// function fib(n){
+
+//      let n1 = 0
+//      let n2 = 1
+//     //  let n = 11
+//     output = ""
+//     for (let i = 1; i <= n; i++) {
+//         output += `${n1} `;
+//         next = n1 + n2;
+//         n1 = n2;
+//         n2 = next;
+//     }
+//     console.log(output)
+// }
+
+// fib(9)
+
+// let a = "153"
+// var b = a.split("")                               //toString().
+// // var c = b.map(Number)
+// console.log(`length:- ${b.length}` )
+// sum = 0
+// console.log(b)
+// // // console.log(b)
+// for(i=0;i<=b.length-1;i++){
+// sum += Math.pow(b[i],b.length)
+// // console.log(sum)
+// }
+// if(a==sum){
+//     console.log("it is a armstrong number ")
+// }
+// else {
+//     console.log("it is not a armstrong number ")
+// }
+// console.log(sum)
+
+
+// i = 0
+// function f1(){
+//     if(i==10){
+//         return
+//     }
+//     else{
+//         console.log(i)
+//         return f1(i++)
+//     }
+// }
+
+// f1()
+
+
+
+
+
+
