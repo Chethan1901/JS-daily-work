5ea691476ffdcb4be1808f18e9dd4c8c33660010
Chethan1901 <chethanreddy.akkati123@gmail.com>
2022-08-18 12:38:14 +0530

    aug18

--- Matrices/Scratch2.js
+++ Matrices/Scratch2.js
@@ -1,4 +1,4 @@
-import { matrix,PrintMatrix } from "./Scratch.js";
+// import { matrix,PrintMatrix } from "./Scratch.js";
 
 // function ADD(){
 //     let matrix1 = matrix()
@@ -50,3 +50,36 @@ import { matrix,PrintMatrix } from "./Scratch.js";
 // }
 // SUB()
 
+
+
+import { matrix,PrintMatrix } from "./Scratch.js";
+function multiplication(){
+       let matrix1 = matrix()
+       PrintMatrix(matrix1)
+       let matrix2 = matrix()
+       PrintMatrix(matrix2)
+       if(matrix1[0].length == matrix2.length ){
+           var matrix3 = new Array(matrix1.length)
+           for(let i=0;i<matrix1.length;i++){
+               matrix3[i] = new Array(matrix2[0].length)
+           }
+//for multiplication
+  // Initializing elements of matrix mult to 0.
+  for(let i=0;i<matrix1.length;i++){
+            for(let j=0;j<matrix2[0].length;j++){
+                matrix3[i][j]=0
+            for (var k = 0; k < matrix1[0].length; k++) {
+                matrix3[i][j]+= matrix1[i][k] * matrix2[k][j];
+                // PrintMatrix(result)
+        }
+     }
+    }
+    //
+    console.log("\n matrix after mulplication:")
+    PrintMatrix(matrix3)
+}
+       else{
+           console.log(`not same`)
+       }
+    }
+    multiplication()
--- Matrices/Scratch3.js
+++ Matrices/Scratch3.js
@@ -1,28 +1,28 @@
 // import { matrix,PrintMatrix } from "./Scratch.js";
-import readlineSync from "readline-sync";
+// import readlineSync from "readline-sync";
 
-function CheckMatrix(){
+// function CheckMatrix(){
 
-let R = readlineSync.questionInt("enter Rows:")
-let C = readlineSync.questionInt("enter Cols:")
-console.log(`given CheckMatrix is in ${R} * ${C}`)
+// let R = readlineSync.questionInt("enter Rows:")
+// let C = readlineSync.questionInt("enter Cols:")
+// console.log(`given CheckMatrix is in ${R} * ${C}`)
 
-let newMatrix = new Array(R)
-for(let i=0;i<newMatrix.length;i++){
-    newMatrix[i] = new Array(C)
-}
+// let newMatrix = new Array(R)
+// for(let i=0;i<newMatrix.length;i++){
+//     newMatrix[i] = new Array(C)
+// }
 
 
-for(let i=0;i<newMatrix.length;i++){
-    for(let j=0;j<C;j++){
-        newMatrix[i][j] = readlineSync.questionInt(`enter element of ${i},${j}:`)
-    }
-}
-console.log(newMatrix)
-return newMatrix;
-}
+// for(let i=0;i<newMatrix.length;i++){
+//     for(let j=0;j<C;j++){
+//         newMatrix[i][j] = readlineSync.questionInt(`enter element of ${i},${j}:`)
+//     }
+// }
+// // console.log(newMatrix)
+// return newMatrix;
+// }
 
-let mat = CheckMatrix()
+// let mat = CheckMatrix()
 // function Check(newMatrix){
 //  let N= []
 // let P= []
@@ -36,56 +36,66 @@ let mat = CheckMatrix()
 //     }
 // }
 // }
-// console.log(`${N} and ${P}`)
+// console.log(`Negative values are:- ${N} & Positive values are:- ${P}`)
 // }
 // Check(mat)
 
 
 // function Prime(newMatrix){
 //     console.log(newMatrix)
-//     console.log(newMatrix[0][0])
 //     for(let i=0;i<newMatrix.length;i++){
 //         for(let j=0;j<newMatrix[0].length;j++){
 
-//            let isPrime=true
-//             // if (newMatrix[i][j] > 1) {
-//                 // console.log(newMatrix[i][j])
-//             for (let i=2; i< newMatrix[i][j]; i++) {
-//             if (newMatrix[i][j] % i == 0) {
-//             isPrime = false;
-//             break;
-//              }
-//               }
-//               if (isPrime) {
-//                  console.log(`${newMatrix[i][j]} is a prime newMatrix[i][j]`);
-//              }
-//              else {
-//                  console.log(`${newMatrix[i][j]} is a not prime newMatrix[i][j]`);
-//               }
-// }
-//                     }
+//         const number = newMatrix[i][j];
+//         let isPrime = true;
+
+//         if (number === 1) {
+//             console.log("1 is neither prime nor composite number.");
+//         }
+
+//         else if (number > 1) {
+
+//             for (let i = 2; i < number; i++) {
+//                 if (number % i == 0) {
+//                     isPrime = false;
+//                     break;
+//                 }
+//             }
+
+//             if (isPrime) {
+//                 console.log(`${number} is a prime number`);
+//             } else {
+//                 console.log(`${number} is a not prime number`);
+//             }
+//         }
+
+//         else {
+//             console.log("The number is not a prime number.");
+//         }
+//         }
 //     }
+// }
 //    Prime(mat)
    
-function Check(newMatrix){
-    for(let i=0;i<newMatrix.length;i++){
-                for(let j=0;j<newMatrix[0].length;j++){
-let a = newMatrix[i][j]
-console.log(a)
-var b = a.toString()   
-console.log(b)               
-let sum = 0
-for(i=0;i<=b.length-1;i++){
-sum += Math.pow(b[i],b.length)
-console.log(sum)               
-}
-if(a==sum){
-    console.log( "it is a armstrong number ")
-}
-else {
-    console.log("it is not a armstrong number")
-}
-}
-}
-}
-Check(mat)
+// function Armstrong(newMatrix){
+//     for(let i=0;i<newMatrix.length;i++){
+//         for(let j=0;j<newMatrix[0].length;j++){
+//         let a = newMatrix[i][j];
+//         // console.log(a)
+//         var b = a.toString()   
+//         // console.log(b)               
+//         let sum = 0
+//         for(i=0;i<=b.length-1;i++){
+//         sum += Math.pow(b[i],b.length)
+//         console.log(sum)               
+//         }
+//         if(a==sum){
+//             console.log( "it is a armstrong number ")
+//         }
+//         else {
+//             console.log("it is not a armstrong number")
+//         }
+//             }
+//         }
+// }
+// Armstrong(mat)
